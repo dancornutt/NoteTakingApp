d4f745f232ea48bdd1795bf4b4c2d433c2525b4f
Dan Cornutt <dacornutt@gmail.com>
2020-11-13 16:45:09 -0800

    Server up and running

--- Develop/server.js
+++ Develop/server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./public/apiRoutes")(app);
-require("./public/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
